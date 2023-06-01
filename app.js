ac3d317c67790e5853ea7832b8a9fa40f6744642
yatcher-1 <khatrirajkumar78@gmail.com>
2023-06-01 15:46:51 +0530

    comming soon adding and deploy

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-       res.render("home");
+       res.render("comming-soon");
 });
 
 app.get("/About-Us", function (req, res) {
