4d73fc94d538516703071b3b38480e03f4f1b384
yatcher-1 <khatrirajkumar78@gmail.com>
2023-06-05 11:50:03 +0530

    brb

--- app.js
+++ app.js
@@ -8,53 +8,45 @@ const PORT = process.env.PORT || 3000;
 const http = require("http");
 const mongoose = require("mongoose");
 
+mongoose.connect("mongodb+srv://Yatcher_01:Jaguar123@new.nufxqpo.mongodb.net/blogDB", {useNewUrlParser:true});
+
 const postSchema = {
   title: String,
   content: String,
   class: String
 };
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const Post = mongoose.model("Post", postSchema);
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-async function run() {
-  await mongoose.connect("mongodb+srv://Yatcher_01:Jaguar123@new.nufxqpo.mongodb.net/blogDB", {
-    useNewUrlParser: true
-  });
-  const Post = mongoose.model("Post", postSchema);
-  await app.get("/", function (req, res) {
-    Post.find({}).then(function (posts) {
+app.get("/", function(req, res){
+    Post.find({}).then(function(posts){
       try {
-        res.render("home", {
-          posts: posts
-        });
+        res.render("home", {posts: posts});
       } catch (error) {
-        console.log(error);
+      console.log(error);
       }
     });
-  }); // Works!
-}
-run();
-
-
+  });
 
 app.get("/About-Us", function (req, res) {
-  res.render("about");
+    res.render("about");
 });
 
 app.get("/Contact", function (req, res) {
-  res.render("contact");
+    res.render("contact");
 });
 
-app.get("/Blogs", function (req, res) {
-  res.render("blogs");
+app.get("/Blogs", function(req, res){
+    res.render("blogs");
 });
 
 
 app.listen(PORT, (req, res) => {
-  console.log(`server started on port ${PORT}`);
+    console.log(`server started on port ${PORT}`);
 });
