86642f453b4a19a0501b27719c92a126c6aa9f74
yatcher-1 <khatrirajkumar78@gmail.com>
2023-06-07 12:07:51 +0530

    update comming

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const _ = require("lodash");
 const PORT = process.env.PORT || 3000;
 const http = require("http");
-const mongoose = require("mongoose");
+
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -15,52 +15,9 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb+srv://Yatcher_01:Jaguar123@new.nufxqpo.mongodb.net/blogDB", {useNewUrlParser:true});
-    const postSchema = {
-      title: String,
-      content: String,
-      class: String
-    };
-    
-    const Post = mongoose.model("Post", postSchema);
-    app.get("/", function(req, res){
-      Post.find({}).then(function(posts){
-        try {
-          res.render("comming-soon", {posts: posts});
-        } catch (error) {
-        console.log(error);
-        }
-      });
-    });
-    app.get("/Blogs", function(req, res){
-      const limit = 3;
-      Post.find({}).limit(limit).then(function(posts){
-        try {
-          res.render("blogs", {postsFound: posts});
-        } catch (error) {
-        console.log(error);
-        }
-      });
-    });
-    app.get("/Blogs/:postId", function(req, res){
-      const requestPostId = req.params.postId;
-        Post.findOne({_id:requestPostId}).then(function(post){
-              res.render("blog", {
-                title: post.title,
-                content: post.content,
-                post: post.class
-              });
-            });
-    });
-    console.log("connection to mong sucssefull");
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-connectDB();
+app.get("/", function (req, res){
+  res.render("comming-soon");
+});
 
 app.get("/Eccomerce-Consultancy", function (req, res) {
   res.render("ecomerce");
