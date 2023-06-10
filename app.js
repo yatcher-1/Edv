0c94a77077f569241e827e9fb29e077ac384957f
yatcher-1 <khatrirajkumar78@gmail.com>
2023-06-10 08:12:56 +0530

    for render update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const _ = require("lodash");
 const PORT = process.env.PORT || 3000;
 const http = require("http");
+const mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -14,10 +15,84 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 
-app.get("/", function(req, res){
-  res.render("comming-soon");
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://Yatcher_01:Jaguar123@new.nufxqpo.mongodb.net/blogDB", {useNewUrlParser:true});
+    const postSchema = {
+      title: String,
+      content: String,
+      class: String
+    };
+    
+    const Post = mongoose.model("Post", postSchema);
+    app.get("/", function(req, res){
+      Post.find({}).then(function(posts){
+        try {
+          res.render("home", {posts: posts});
+        } catch (error) {
+        console.log(error);
+        }
+      });
+    });
+    app.get("/Blogs", function(req, res){
+      const limit = 18;
+      Post.find({}).limit(limit).then(function(posts){
+        try {
+          res.render("blogs", {postsFound: posts});
+        } catch (error) {
+        console.log(error);
+        }
+      });
+    });
+    app.get("/Blogs/:postId", function(req, res){
+      const requestPostId = req.params.postId;
+        Post.findOne({_id:requestPostId}).then(function(post){
+              res.render("blog", {
+                title: post.title,
+                content: post.content,
+                post: post.class
+              });
+            });
+    });
+    console.log("connection to mong sucssefull")
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectDB();
+
+app.get("/Eccomerce-Consultancy", function (req, res) {
+  res.render("ecomerce");
 });
 
+app.get("/Digital-Marketing", function (req, res) {
+  res.render("digital");
+});
+
+app.get("/Influencer-Marketing", function (req, res) {
+  res.render("influencer");
+});
+
+app.get("/Web-Dev", function (req, res) {
+  res.render("webdesign");
+});
+
+app.get("/About-Us", function (req, res) {
+    res.render("about");
+});
+
+app.get("/Contact", function (req, res) {
+    res.render("contact");
+});
+
+app.get("/Career", function (req, res) {
+    res.render("career");
+});
+
+app.get("/Partner", function (req, res) {
+    res.render("partner");
+});
 
 app.listen(PORT, (req, res) => {
     console.log(`server started on port ${PORT}`);
